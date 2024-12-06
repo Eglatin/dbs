5a0d36a9b3ae688f0e296042348ee9cd835c1197
fatjonpopaj <117153009+fatjonpopaj@users.noreply.github.com>
2024-12-06 20:54:00 +0100

    Update Movie.java

--- Bonusaufgabe2/src/main/java/de/hsh/dbs2/imdb/entities/Movie.java
+++ Bonusaufgabe2/src/main/java/de/hsh/dbs2/imdb/entities/Movie.java
@@ -7,6 +7,7 @@ import java.util.Set;
 @Entity
 public class Movie {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private int year;
